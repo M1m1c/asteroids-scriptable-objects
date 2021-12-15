7367a7ba7334ad033d7959ba0218852b97d5ee3e
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-15 13:55:23 +0100

    Added scriptable event tath takes type splitdata

---
+++ Assets/_Game/Assignment/Scripts/ScriptableEventSplitData.cs
@@ -0,0 +1,13 @@
+﻿using Asteroids;
+using UnityEngine;
+
+namespace DefaultNamespace.ScriptableEvents
+{
+    [CreateAssetMenu(fileName = "new ScriptableEventSplitData", menuName = "ScriptableObjects/ScriptableEvent-SplitData", order = 0)]
+    public class ScriptableEventSplitData : ScriptableEvent<SplitData>
+    {
+        
+    }
+
+  
+}
