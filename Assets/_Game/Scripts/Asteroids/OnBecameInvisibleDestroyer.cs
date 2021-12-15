88ba5a92a6387808dc29281e0544d435cbb6cbd8
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-15 13:57:16 +0100

    added referecnce to asteroid set and use that calsses destroy fucntion if it is set

--- Assets/_Game/Scripts/Asteroids/OnBecameInvisibleDestroyer.cs
+++ Assets/_Game/Scripts/Asteroids/OnBecameInvisibleDestroyer.cs
@@ -4,10 +4,21 @@ namespace Asteroids
 {
     public class OnBecameInvisibleDestroyer : MonoBehaviour
     {
+        [SerializeField] private AsteroidSet _asteroidSet;
         [SerializeField] private GameObject _toDestroy;
-        
+
         private void OnBecameInvisible()
         {
+            if (_asteroidSet)
+            {
+                var asteroid = _toDestroy.GetComponent<Asteroid>();
+                if (asteroid) 
+                {
+                    _asteroidSet.DestroyAsteroid(asteroid);
+                }
+            }
+
+            if (!_toDestroy) { return; }
             Destroy(_toDestroy);
         }
     }
