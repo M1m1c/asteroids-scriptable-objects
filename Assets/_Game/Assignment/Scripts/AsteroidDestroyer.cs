13b9418ce6261d0ae023f062436576b512f12712
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-15 17:28:20 +0100

    removed comments and made sue to reduce size when calling for split

--- Assets/_Game/Assignment/Scripts/AsteroidDestroyer.cs
+++ Assets/_Game/Assignment/Scripts/AsteroidDestroyer.cs
@@ -7,33 +7,25 @@ namespace Asteroids
     {
         [SerializeField] private ScriptableEventSplitData _OnAsteroidSplit;
         [SerializeField] private AsteroidSet _asteroidSet;
+        [SerializeField] private float _destructionThreshold = 0.3f;
         public void OnAsteroidHitByLaser(int asteroidId)
         {
             if (!_asteroidSet) { return; }
 
             var hitAsteroid = _asteroidSet.GetAsteroid(asteroidId);
             if (!hitAsteroid) { return; }
-            // Get the asteroid
+
             var size = hitAsteroid.GetSize();
-            if (size > 0.3)
+            if (size > _destructionThreshold)
             {
-                //Split asteroid by spawning two new ones with half the size
                 if (_OnAsteroidSplit) 
                 {
                     var destroyPos = hitAsteroid.transform.position;
-                    _OnAsteroidSplit.Raise(new SplitData(destroyPos, size));
+                    _OnAsteroidSplit.Raise(new SplitData(destroyPos, size/2f));
                 }
             }
 
             _asteroidSet.DestroyAsteroid(hitAsteroid);
-
-
-
-            // Check if big or small
-
-            // if small enough, we Destoy
-
-            // if it's big, we split it up.
         }
 
     }
