d45e8d9483de9d2956c09928dd35b3a35d3ebf46
L B O/(M1m1c) <ludvigbaumolss@hotmail.se>
2021-12-16 14:41:06 +0100

    Added OnAsteroidBecameInvisibleDestroyer

---
+++ Assets/_Game/Assignment/Scripts/OnAsteroidBecameInvisibleDestroyer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class OnAsteroidBecameInvisibleDestroyer : MonoBehaviour
+    {
+        [SerializeField] private AsteroidSet _asteroidSet;
+        [SerializeField] private GameObject _toDestroy;
+
+        private void OnBecameInvisible()
+        {
+            if (_asteroidSet)
+            {
+                var asteroid = _toDestroy.GetComponent<Asteroid>();
+                if (asteroid) 
+                {
+                    _asteroidSet.DestroyAsteroid(asteroid);
+                }
+            }
+
+            if (!_toDestroy) { return; }
+            Destroy(_toDestroy);
+        }
+    }
+}
